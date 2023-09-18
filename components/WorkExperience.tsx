339c2726eddebefc3ddab9c4ff9a4ea60aaf6c2b
Mahan Asadi <mahan.asadiiii@gmail.com>
2023-09-18 18:49:03 +0330

    add key to map function

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -7,9 +7,9 @@ const WorkExperinece = () => {
         <div className="flex justify-between gap-5">
             <div className="w-7 bg-gradient-to-b from-violet-800 to-violet-100 rounded-full"></div>
             <div className="flex flex-col justify-between gap-10 basis-[95%] py-4">
-                {WorkItems.map((item) => {
+                {WorkItems.map((item,index) => {
                     return (
-                        <div className="flex flex-col md:w-1/2 group ">
+                        <div key={index} className="flex flex-col md:w-1/2 group ">
                             <div className="md:group-hover:-translate-x-60 group-hover:scale-105 md:group-hover:scale-125 ease-in-out duration-500 group-hover:shadow-2xl group-hover:p-5 rounded-xl flex flex-col gap-2 group-hover:gap-4 group-hover:bg-violet-500 group-hover:text-white ">
                                 <div className="flex flex-col md:flex-row gap-2 md:gap-5 md:items-baseline ">
                                     <div className="flex items-center gap-1">
