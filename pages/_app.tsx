8360ed1f0808e8f7d5242dcbfa5fc04140ff239e
mahan <mahan.asadiiii@gmail.com>
2024-05-05 12:34:22 +0330

    connect it to Plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import "../styles/fonts.css"
 import type { AppProps } from 'next/app'
 import Head from 'next/head';
 import { ThemeProvider } from '../context/ThemeContext';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,8 +12,15 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="theme-color" content="#7C3AED" />
       </Head>
       <ThemeProvider>
+        <Script
+        async
+        defer
+        data-domain="mahan-asadi.ir" 
+        src="https://plausible.io/js/script.js"
+        />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
   );
 }
+
